3b0bc8ee5c06b846552fe0dff4c7dcbc9c5737bc
marocks31 <mollyarisley@gmail.com>
2022-04-22 15:49:37 -0500

    added routes from sales controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  get "/sales" => "sales#index"
+  post "/sales" => "sales#create"
+  get "/sales/:id" => "sales#show"
+  patch "/sales/:id" => "sales#update"
+  delete "/sales/:id" => "sales#destroy"
+
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
   
