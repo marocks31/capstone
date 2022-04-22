8f552f18db79f9799a909e18e5f05627c7224944
marocks31 <mollyarisley@gmail.com>
2022-04-22 15:37:25 -0500

    finished sales controller

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -6,6 +6,18 @@ def  index
 end 
 
 def create
+  sale = Sale.new(
+    # user id?
+    title: params[:title],
+    description: params[:description],
+    start_date: params[:start_date],
+    time: params[:time],
+    address: params[:address],
+    picture: params[:picture],
+    end_date: params[:end_date]
+  )
+  sale.save
+  render json: sale.as_json
 end
 
 def show
@@ -14,6 +26,16 @@ def show
 end
 
 def update
+  sale = Sale.find_by(id: params[:id])
+  sale.title = params[:title] || sale.title
+  sale.description = params[:description] || sale.description
+  sale.start_date = params[:start_date] || sale.start_date
+  sale.time = params[:time] || sale.time
+  sale.address = params[:address] || sale.address
+  sale.picture = params[:picture] || sale.picture
+  sale.end_date = params[:end_date] || sale.end_date
+  sale.save
+  render json: sale.as_json
 end
 
 def destroy
